fa04820a6847c29841083ddda5caa56aa1729295
BuddyZhang1 <buddy.zhang@aliyun.com>
2020-05-03 21:54:27 +0800

    linux 2.6.13

--- main.c
+++ main.c
@@ -204,7 +204,7 @@ static int BiscuitOS_memory_probe(struct platform_device *pdev)
 
 	start_kernel_bs();
 
-	printk("Hello BiscuitOS: Linux 2.6.12.6 MMU\n");
+	printk("Hello BiscuitOS: Linux 2.6.13 MMU\n");
 
 	return 0;
 }
